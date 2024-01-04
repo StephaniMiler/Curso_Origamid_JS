0567bb23f186bc7ce81b038ff26808ab1c49f54f
Stephani Sara Miler <107444034+StephaniMiler@users.noreply.github.com>
2024-01-04 19:52:43 -0300

    Add files via upload

---
+++ 002.Tipos de dados/script.js
@@ -0,0 +1,14 @@
+var nome = 'Stephani';
+var sobrenome = 'Miler'
+var idade = 23;
+var time;
+var time2 = null;
+var simbolo = Symbol()
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof time);
+console.log(typeof time2);
+console.log(typeof simbolo);
+
+console.log(nome + ' ' + sobrenome);
